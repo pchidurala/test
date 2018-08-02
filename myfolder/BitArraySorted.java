ddb9175848ebf2018c4cebd2c09f67d24fe59a34
pchidurala <prashanthi.chidurala@gmail.com>
2018-08-02 10:13:18 -0700

    added comment and changed input array

--- myfolder/BitArraySorted.java
+++ myfolder/BitArraySorted.java
@@ -1,7 +1,8 @@
+//Get number of 0's and # of 1's from a sorted BitArray
 class BitArraySorted {
  public static void main(String[] args) {
    System.out.println("Hello world!");
-   int[] a = new int[]{};
+   int[] a = new int[]{0, 0, 0, 1, 1, 1, 1};
    int s = 0;
    int e = a.length - 1;
    int m = 0;
